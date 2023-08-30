c4f2f36d8dad223b06e2be8a8837be36c0223cbf
imba97 <mail@imba97.cn>
2023-08-30 08:37:13 +0800

    perf: 代码执行问题

--- code/js-57.js
+++ code/js-57.js
@@ -1,11 +1,11 @@
 (function () {
-  if (continueClickTimer) {
-    clearInterval(continueClickTimer);
-    continueClickTimer = undefined;
-    console.log('关闭')
+  if (typeof window.continueClickTimer !== 'undefined') {
+    clearInterval(window.continueClickTimer);
+    window.continueClickTimer = undefined;
+    console.log("关闭");
     return;
   }
 
-  console.log('开启')
-  var continueClickTimer = setInterval(/* TODO: 点击事件 */ () => {}, 10000);
-});
+  console.log("开启");
+  window.continueClickTimer = setInterval(/* TODO: 点击事件 */ () => {}, 10000);
+})();
