957cacdb246b4997da015a82af404596337b6b44
imba久期 <imba97@qq.com>
2023-08-30 19:26:58 +0800

    perf: 继续等待代码优化

--- code/js-57.js
+++ code/js-57.js
@@ -1,11 +1,13 @@
-(function () {
-  if (typeof window.continueClickTimer !== 'undefined') {
-    clearInterval(window.continueClickTimer);
-    window.continueClickTimer = undefined;
+(function (w) {
+  if (typeof w.continueClickTimer !== 'undefined') {
+    clearInterval(w.continueClickTimer);
+    w.continueClickTimer = undefined;
     console.log("关闭");
     return;
   }
 
   console.log("开启");
-  window.continueClickTimer = setInterval(window.ec.wait.continueWait, 10000);
-})();
+  w.continueClickTimer = setInterval(() => {
+    w.ec.wait.continueWait()
+  }, 10000);
+})(window);
